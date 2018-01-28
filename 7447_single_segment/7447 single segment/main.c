325064e2cbcbdcbb7e593207ce8cb66ddb652ac8
ishrak ahmed <sam@example.comishrak.ahmed2@gmail.com>
2018-01-28 22:11:09 +0600

    single segment counter

---
+++ 7447_single_segment/7447 single segment/main.c
@@ -0,0 +1,33 @@
+/*
+ * 7447 counter.c
+ *
+ * Created: 23-Oct-17 9:44:01 PM
+ * Author : ishrak
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 16000000UL // 16 MHz clock speed
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	char output[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09};
+	DDRC= 0x0f;
+	
+	while (1)
+	{
+		
+		for(int i=0;i<=9;i++)
+		{
+			
+			PORTC = output[i];
+			_delay_ms(500);
+		}
+		
+	}
+
+	}
+
